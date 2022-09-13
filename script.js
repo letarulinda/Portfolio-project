6ce0e50997a2b6a3dff8af3c3006a98ca57f918d
letarulinda <letarulinda@gmail.com>
2022-09-13 22:35:10 +0300

    modify script.js file

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menuIcon = document.querySelector
   ('.nav-menu');
+const navList = document.querySelector('.nav-list')
 const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
+  navList.classList.toggle('active')
   navbar.classList.toggle('change');
 });
 
+navList.addEventListener('click', () => {
+  navList.style.display = 'none'
+  navbar.classList.remove('change');
 
+});
